24b1cc8c08d1b01cc4127b000ecbc5a4758ee3cc
sumita <mel1cob@in.bosch.com>
2026-08-24 10:42:50 +0200

    RED: Add off state test

--- tests/test_heating_controller.py
+++ tests/test_heating_controller.py
@@ -4,3 +4,9 @@ def test_heating_when_temp_below_target_temp():
     controller = HeatingController(target_temp=21)
     result = controller.get_action(current_temp=19)
     assert result == 'HEATING'
+
+def test_off_when_temp_greaterorequalto_target_temp():
+    controller = HeatingController(target_temp=22)
+    result = controller .get_action(current_temp=22)
+    assert result == "OFF"
+

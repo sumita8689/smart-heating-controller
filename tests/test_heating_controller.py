f94191deb5b1257d0e63cfba452c75e386f3e5c5
sumita <mel1cob@in.bosch.com>
2026-08-24 10:52:13 +0200

    RED: Add hysteresis heating test

--- tests/test_heating_controller.py
+++ tests/test_heating_controller.py
@@ -10,3 +10,7 @@ def test_off_when_temp_greaterorequalto_target_temp():
     result = controller .get_action(current_temp=22)
     assert result == "OFF"
 
+def test_heating_when_temp_below_lower_hysteresis_limit():
+    controller = HeatingController(target_temp=21,hysteresis=0.5)
+    result = controller.get_action(current_temp=20.7)
+    assert result == "OFF"

f0acac7e7feba34980100e0c2baba32ea5e1e731
sumita <mel1cob@in.bosch.com>
2026-08-24 11:18:50 +0200

    GREEN:Add temperature validation

--- heating_controller.py
+++ heating_controller.py
@@ -4,6 +4,8 @@ class HeatingController:
         self.hysteresis = hysteresis
         self.action = "OFF"
     def get_action(self,current_temp):
+        if current_temp >60 or current_temp < -50:
+            raise ValueError("Invalid Temperature")
         if current_temp < (self.target_temp- self.hysteresis):
             self.action = "HEATING"
         elif current_temp >= self.target_temp:

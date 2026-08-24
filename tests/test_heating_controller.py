d9bf9c9cabe0c88d3beebf1b7cea53485a98b558
sumita <mel1cob@in.bosch.com>
2026-08-24 12:16:29 +0200

    RED: Add AWAY mode tests

--- tests/test_heating_controller.py
+++ tests/test_heating_controller.py
@@ -52,3 +52,15 @@ def test_switching_eco_comfort_mode():
     controller.set_mode("COMFORT")
     result = controller.get_action(current_temp=19)
     assert result == "HEATING"
+
+def test_off_away_mode():
+    controller = HeatingController(target_temp=21,hysteresis=0.5)
+    controller.set_mode("AWAY")
+    result = controller.get_action(current_temp=16)
+    assert result=="OFF"
+
+def test_heating_away_mode():
+    controller = HeatingController(target_temp=21,hysteresis=0.5)
+    controller.set_mode("AWAY")
+    result = controller.get_action(current_temp=15)
+    assert result=="HEATING"
